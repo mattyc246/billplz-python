6f968f7fa5218482152784db09befd15ddcb0838
Matthew Cross <mattyc246@gmail.com>
2020-04-24 12:40:16 +0800

    base client setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'billplz-python',
+  packages = find_packages(),
+  version = '1.0.0',
+  license='MIT',
+  description = 'Billplz Client Interface',
+  author = 'Matthew Cross',
+  author_email = 'mattyc246@gmail.com',
+  url = 'https://github.com/mattyc246/billplz-python',
+  download_url = '',
+  keywords = ['billplz', 'payment', 'payment gateway'],
+  include_package_data = True,
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
