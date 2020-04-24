8c8c3a183c8392e94c6b408ae1a52caf75656ee4
Matthew Cross <mattyc246@gmail.com>
2020-04-24 15:42:30 +0800

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'billplz-python',
   packages = find_packages(),
-  version = '1.0.0',
+  version = '1.0.1',
   license='MIT',
   description = 'Billplz Client Interface',
   author = 'Matthew Cross',
