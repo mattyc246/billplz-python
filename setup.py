9a0ed67acdd67c3712ec9be2b5e80a545dd3c1dd
Matthew Cross <mattyc246@gmail.com>
2020-05-05 13:31:27 +0800

    update version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 setup(
   name = 'billplz-python',
   packages = find_packages(),
-  version = '1.0.1',
+  version = '1.0.2',
   license='MIT',
   description = 'Billplz Client Interface',
   author = 'Matthew Cross',
   author_email = 'mattyc246@gmail.com',
   url = 'https://github.com/mattyc246/billplz-python',
-  download_url='https://github.com/mattyc246/billplz-python/archive/1.0.1.tar.gz',
+  download_url='https://github.com/mattyc246/billplz-python/archive/1.0.2.tar.gz',
   keywords = ['billplz', 'payment', 'payment gateway'],
   include_package_data = False,
   classifiers=[
